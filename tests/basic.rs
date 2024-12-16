b6e87033e1d3a1b8273bbd4292b4c0ab60baff8b
al amoda <alamoda@tutamail.com>
2024-12-16 17:59:42 -0500

    tokio::test singlethread runtime

--- tests/basic.rs
+++ tests/basic.rs
@@ -3,8 +3,9 @@ use tokio::{
     time::{sleep, Duration},
 };
 
-#[tokio::test(flavor = "multi_thread")]
-async fn async_multitask_sanity_check() {
+// testing with tokio::test default
+#[tokio::test]
+async fn async_singlethread_sanity_check() {
     let mut set: JoinSet<()> = JoinSet::new();
     let the_vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
     for _v in the_vec {
@@ -12,7 +13,23 @@ async fn async_multitask_sanity_check() {
             sleep(Duration::from_millis(200)).await;
         });
     }
-    sleep(Duration::from_millis(2000)).await;
+    sleep(Duration::from_millis(1000)).await;
+    assert!(!set.is_empty());
+    assert!(set.len() == 10);
+    set.join_all().await;
+}
+
+// testing with tokio::test default
+#[tokio::test(flavor = "multi_thread")]
+async fn async_multithread_sanity_check() {
+    let mut set: JoinSet<()> = JoinSet::new();
+    let the_vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    for _v in the_vec {
+        set.spawn(async move {
+            sleep(Duration::from_millis(200)).await;
+        });
+    }
+    sleep(Duration::from_millis(1000)).await;
     assert!(!set.is_empty());
     assert!(set.len() == 10);
     set.join_all().await;
