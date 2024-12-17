f25ce24cec7e1fb92ae9cd3a9d70418edeb3357f
nachog00 <nachogeo@zingolabs.org>
2024-12-17 17:10:33 -0300

    Added logging registry on main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,20 @@ use tokio::net::TcpListener;
 
 use tower_http::{services::ServeDir, trace::TraceLayer};
 
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
 #[tokio::main]
 async fn main() {
     println!("program start!");
+
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     let listener = TcpListener::bind("127.0.0.1:3333")
         .await
