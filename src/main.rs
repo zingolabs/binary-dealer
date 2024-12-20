74abd60c6f5fc8b68766f05bfccb4771ce663ab3
nachog00 <nachogeo@zingolabs.org>
2024-12-19 21:33:47 -0300

    Removed old 'program start' log. We probably want this handled by `tracing`. Not sure yet

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,6 @@ use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
-    println!("program start!");
-
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
