688816684d96bec23784157f62010003a3e0de3c
al amoda <alamoda@tutamail.com>
2024-12-29 23:07:48 -0500

    add binary names to router path

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,12 @@ async fn main() {
 
     let app: Router = Router::new()
         .nest_service("/getme", ServeDir::new("assets/getme"))
-        .nest_service("/zcashd", ServeDir::new("assets/getme"))
+        .nest_service("/lightwalletd", ServeDir::new("assets/getme"))
         .nest_service("/zainod", ServeDir::new("assets/getme"))
+        .nest_service("/zcashd", ServeDir::new("assets/getme"))
+        .nest_service("/zcash-cli", ServeDir::new("assets/getme"))
+        .nest_service("/zebrad", ServeDir::new("assets/getme"))
+        .nest_service("/zingo-cli", ServeDir::new("assets/getme"))
         .layer(TraceLayer::new_for_http());
     tracing::info!("set app as : {:?}", &app);
 
